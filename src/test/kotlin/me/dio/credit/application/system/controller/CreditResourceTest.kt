d37d3ff7362a7b26b7417e2436bad16aff7bf6bc
Vitor Silva <vitor.azevedo009@gmail.com>
2024-01-03 11:11:22 -0300

    Create unit test for 'create credit' feature

--- src/test/kotlin/me/dio/credit/application/system/controller/CreditResourceTest.kt
+++ src/test/kotlin/me/dio/credit/application/system/controller/CreditResourceTest.kt
@@ -2,9 +2,10 @@ package me.dio.credit.application.system.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import me.dio.credit.application.system.dto.request.CreditDto
-import me.dio.credit.application.system.dto.request.CustomerDto
 import me.dio.credit.application.system.entity.Customer
+import me.dio.credit.application.system.repository.CreditRepository
 import me.dio.credit.application.system.repository.CustomerRepository
+import me.dio.credit.application.system.service.impl.CreditService
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -28,7 +29,13 @@ import java.time.LocalDate
 class CreditResourceTest {
 
     @Autowired
+    private lateinit var creditRepository: CreditRepository
+
+    @Autowired
     private lateinit var customerRepository: CustomerRepository
+
+    @Autowired
+    private lateinit var creditService: CreditService
 
     @Autowired
     private lateinit var mockMvc: MockMvc
@@ -40,40 +47,40 @@ class CreditResourceTest {
         const val URL: String = "/api/credits"
     }
     @BeforeEach
-    fun setup() = customerRepository.deleteAll()
+    fun setup() = creditRepository.deleteAll()
 
     @AfterEach
-    fun tearDown() = customerRepository.deleteAll()
+    fun tearDown() = creditRepository.deleteAll()
 
     @Test
     fun `should create credit`(){
         // given
         val customCreditDTO: CreditDto = builderCreditDto()
+        customerRepository.save(Customer(id = 1L, email = "vitor@vitor.com"))
         val valueAsString: String = objectMapper.writeValueAsString(customCreditDTO)
+        creditService.save(customCreditDTO.toEntity())
+
         // when
         mockMvc.perform(
-            MockMvcRequestBuilders.post(CreditResourceTest.URL)
+            MockMvcRequestBuilders.post(URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(valueAsString)
 
         )// then
-            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Cami"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Cavalcante"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value("28475934625"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("camila@email.com"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.income").value("1000.0"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("000000"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Rua da Cami, 123"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.creditCode").isNotEmpty())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.creditValue").value(0))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.numberOfInstallment").value(48))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("IN_PROGRESS"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.emailCustomer").value("vitor@vitor.com"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.incomeCustomer").value("0.0"))
             .andDo(MockMvcResultHandlers.print())
-
     }
 
     private fun builderCreditDto(
-        creditValue: BigDecimal = BigDecimal(0.4),
-        dayFirstOfInstallment: LocalDate = LocalDate.now(),
-        numberOfInstallments: Int = 24,
-        customerId: Long = 1
+        creditValue: BigDecimal = BigDecimal(0),
+        dayFirstOfInstallment: LocalDate = LocalDate.now().plusDays(5),
+        numberOfInstallments: Int = 48,
+        customerId: Long = 1L
     ) = CreditDto(
         creditValue = creditValue,
         dayFirstOfInstallment = dayFirstOfInstallment,
