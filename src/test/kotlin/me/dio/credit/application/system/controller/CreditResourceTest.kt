71e6257ce1c9d56a6cfb4235c6482a5425986224
Vitor Silva <vitor.azevedo009@gmail.com>
2024-01-03 10:35:44 -0300

    Create file and initial configuration of CreditResourceTest

---
+++ src/test/kotlin/me/dio/credit/application/system/controller/CreditResourceTest.kt
@@ -0,0 +1,83 @@
+package me.dio.credit.application.system.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import me.dio.credit.application.system.dto.request.CreditDto
+import me.dio.credit.application.system.dto.request.CustomerDto
+import me.dio.credit.application.system.entity.Customer
+import me.dio.credit.application.system.repository.CustomerRepository
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.math.BigDecimal
+import java.time.LocalDate
+
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@ContextConfiguration
+class CreditResourceTest {
+
+    @Autowired
+    private lateinit var customerRepository: CustomerRepository
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
+
+    companion object {
+        const val URL: String = "/api/credits"
+    }
+    @BeforeEach
+    fun setup() = customerRepository.deleteAll()
+
+    @AfterEach
+    fun tearDown() = customerRepository.deleteAll()
+
+    @Test
+    fun `should create credit`(){
+        // given
+        val customCreditDTO: CreditDto = builderCreditDto()
+        val valueAsString: String = objectMapper.writeValueAsString(customCreditDTO)
+        // when
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(CreditResourceTest.URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(valueAsString)
+
+        )// then
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Cami"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Cavalcante"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.cpf").value("28475934625"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("camila@email.com"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.income").value("1000.0"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.zipCode").value("000000"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Rua da Cami, 123"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+            .andDo(MockMvcResultHandlers.print())
+
+    }
+
+    private fun builderCreditDto(
+        creditValue: BigDecimal = BigDecimal(0.4),
+        dayFirstOfInstallment: LocalDate = LocalDate.now(),
+        numberOfInstallments: Int = 24,
+        customerId: Long = 1
+    ) = CreditDto(
+        creditValue = creditValue,
+        dayFirstOfInstallment = dayFirstOfInstallment,
+        numberOfInstallments = numberOfInstallments,
+        customerId = customerId
+    )
+}
